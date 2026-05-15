2592d6f2423d1d740939330b6c5cf48491785a41
kuljeetsinhparmar <164808237+kuljeetsinhparmar@users.noreply.github.com>
2026-05-15 13:08:27 +0530

    Update API base URL to production endpoint

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({ 
-  baseURL: process.env.REACT_APP_API_URL || '/api' 
+  baseURL: 'https://inventory-management-3i4o.onrender.com/api' 
 });
 
 API.interceptors.request.use((config) => {
